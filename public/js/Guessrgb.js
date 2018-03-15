0325609d143145f1681e3d75c2354437ccc7f416
Kaung Yang <kky006@ucsd.edu>
2018-03-15 12:30:48 -0700

    added position generator

--- public/js/Guessrgb.js
+++ public/js/Guessrgb.js
@@ -1,9 +1,11 @@
 let color_R = 0; 
 let color_G = 0; 
 let color_B = 0; 
+let correctPos = 0; 
 $( document ).ready(function() { 
 	fillRGB(); // fills RGB
 	presentNumber(); // writes to the screen
+	fillButtons(); // 
 
 
 }); 
@@ -12,7 +14,15 @@ function generateColorNumber() { // generates a number from 0 - 255, a total of 
     var ret = Math.floor((Math.random() * 256)); 
     return ret;
 }
-
+function generateButtonNumber() { 
+	correctPos = Math.floor((Math.random() * 8)); 
+    return correctPos;
+}
+function fillButtons() {
+  var pos = generateButtonNumber(); 
+  console.log("this is the position of correct button"); 
+  console.log(pos); 
+}
 function fillRGB() {
 	color_R = generateColorNumber(); 
 	color_B = generateColorNumber(); 
@@ -27,3 +37,5 @@ function presentNumber() {
 	document.getElementById("color_G").innerHTML = color_G.toString(); 
 	document.getElementById("color_B").innerHTML = color_B.toString(); 
 }
+
+fillButtons
